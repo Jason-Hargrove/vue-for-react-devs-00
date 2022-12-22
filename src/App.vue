83fe2d553601a31dac892eaf2457e16850f049a2
Jason Hargrove <rijaha@gmail.com>
2022-12-22 07:51:06 -0600

    my first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref } from 'vue'
+import ProductDisplay from '@/components/ProductDisplay.vue'
+import useCart from '@/composables/useCart.js'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const { cartCount, updateCart } = useCart()
+const premium = ref(true)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  
+<template>
+  <div class="nav-bar"></div>
+  <div class="cart">Cart({{ cartCount }})</div>
+  <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
+</template>
---
+++ src/components/ReviewList.vue
@@ -0,0 +1,24 @@
+<script setup>
+// import { useSlots } from 'vue'
+
+// eslint-disable-next-line no-undef
+defineProps({
+  reviews: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="review-container">
+    <h3><slot name="heading"></slot></h3>
+    <ul>
+      <li v-for="(review, index) in reviews" :key="index">
+        <slot name="item" :review="review">
+          <span>{{ review.name }}</span>: <span>{{ review.content }}</span>
+        </slot>
+      </li>
+    </ul>
+  </div>
+</template>
