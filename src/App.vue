ebb7a0a7038f4ee13231893366cdb9e1fdfec9aa
Jason Hargrove <rijaha@gmail.com>
2022-12-23 09:09:27 -0600

    readjusted code for next lesson

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <script setup>
 import { ref } from 'vue'
-import ProductDisplay from '@/components/ProductDisplay.vue'
-import useCart from '@/composables/useCart.js'
+import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
-const { cartCount, updateCart } = useCart()
-const premium = ref(true)
+const product = ref('Socks')
+const image = ref(socksGreenImage)
+const inStock = ref(false)
+  
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+
+const variants = ref([
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage },
+])
+
+const cart = ref(0)
+
+const addToCart = () => cart.value += 1
+
+const updateImage = (variantImage) => {
+  image.value = variantImage
+}
 
 </script>
   
 <template>
   <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cartCount }})</div>
-  <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
+  <div class="cart">Cart({{ cart }})</div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">    
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details" v-bind:key="detail">{{ detail }}</li>
+        </ul>
+        <div 
+          v-for="variant in variants" 
+          :key="variant.id"
+          @mouseover="updateImage(variant.image)"
+          class="color-circle"
+          :style="{ backgroundColor: variant.color }"
+        >
+        </div>
+        <button
+          class="button" 
+          :class="{ disabledButton: !inStock }"
+          :disabled="!inStock"
+          v-on:click="addToCart"
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
