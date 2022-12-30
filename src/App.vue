1864c673aba160dbce041a043231b53932f189c0
Jason Hargrove <rijaha@gmail.com>
2022-12-29 22:38:20 -0600

    creating a more ambitious composable

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <script setup>
-import { ref, onMounted } from "vue"
-import ProductDisplay from "@/components/ProductDisplay.vue"
+import { ref } from "vue";
+import ProductDisplay from "@/components/ProductDisplay.vue";
+import useCart from '@/composables/useCart.js'
 
-const cart = ref([])
-const premium = ref(true)
-
-onMounted(() => {
-  fetch('http://localhost:3000/cart')
-  .then(resp => resp.json())
-  .then(data => cart.value = data.content)
-})
-
-const updateCart = (id) => {
-  cart.value.push(id)
-}
-
+const { cartCount, updateCart } = useCart();
+const premium = ref(true);
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cart.length }})</div>
+  <div class="cart">Cart({{ cartCount }})</div>
   <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
 </template>
